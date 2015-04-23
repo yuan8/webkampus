32ff26c00ec4ccc82d2d73c7d536bf5a1dccc7b5
yuan8 <yuanputra9@gmail.com>
2015-04-23 22:34:36 +0700

    index halaman awal all with css

---
+++ index.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+	<title></title>
+
+<style type="text/css">
+
+*{
+	margin: 0px;
+}
+body{
+	background-color: rgb(241, 241, 241);
+}
+nav{
+	float: left;
+	position: fixed;
+	z-index: 10;
+	width: 100%;
+	height: 80px;
+	background-color: #fff;
+	border-bottom: 1px solid #D8D8D8;
+}
+#ver-nav{
+	width: 200px;
+	float: right;
+	min-height:666px;
+	background-color: #fff;
+	border-left: 1px solid #D8D8D8;  
+}
+#log-menu{
+	width: 101px;
+	border-left: 1px solid #D8D8D8; 
+	height: 80px;
+	float: right;
+}
+#con-index{
+	margin-left:95px;
+    width: 950px;
+    float: left;
+    border: 1px solid #D8D8D8;
+    background-color: #fff;
+    height: 666px;
+    margin-top: 80px;
+
+}
+.col-left-10{
+	padding: 7px;
+	float: left;
+	width: 100%;
+}
+.col-left-7{
+	padding: 7px;
+	float: left;
+	width: 70%
+}
+.col-left-5{
+	padding: 7px;
+	float: left;
+	width: 50%;
+}
+
+.input{
+
+	border: 1px solid #D8D8D8;
+}
+.search{
+
+	}
+.center{
+	float: none;
+	margin-right: auto;
+	margin-left: auto;
+}
+</style>
+
+</head>
+<body>
+<!-- nav -->
+	<nav>
+		<div id="ver-nav">
+			<header class="col-left-10">
+				<img src="b.png">
+			</header>	
+
+		</div>
+		<!-- log menu -->
+		<div id="log-menu"></div>
+		<!-- hr menu -->
+		<div class="col-left-5 ">
+			<input type="text" class=" col-left-7 input search">
+			<button class="">Search</button>
+		</div>
+
+	</nav>
+
+	<div id="con-index">
+
+
+
+
+	</div>
+
+
+
+</body>
+</html>
